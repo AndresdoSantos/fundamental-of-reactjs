513ea08bf57367f1849173f85995d5f5a61bfbcc
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-02 17:26:21 -0300

    feat: header, apply css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+
+import './styles.css';
 
 export function App() {
   return (
     <>
-      <h1>Feed</h1>
+      <Header />
+
       <Post
         author="Andres"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure culpa fugit eaque! Eos tempora maiores praesentium magnam iure quisquam, non cum error. Repellendus sapiente a temporibus doloribus eaque, aliquid tempore."
