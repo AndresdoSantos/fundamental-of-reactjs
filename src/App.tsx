6c2f0aef133dabf2372289ac58fc8622602fcaa7
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2022-06-02 17:52:18 -0300

    feat: global.css, apply global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { Header } from './components/Header';
-import { Post } from './Post';
 
-import './styles.css';
+import './global.css';
 
 export function App() {
   return (
     <>
       <Header />
-
-      <Post
-        author="Andres"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure culpa fugit eaque! Eos tempora maiores praesentium magnam iure quisquam, non cum error. Repellendus sapiente a temporibus doloribus eaque, aliquid tempore."
-      />
     </>
   );
 }
